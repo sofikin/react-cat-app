3aba2faf25e4d83890dc1f137253de8929a33515
sofikin <sofia.kalpin@khk.ee>
2022-10-17 15:50:30 +0300

    "Added API key and Image component"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Box from "@mui/material/Box";
 import NavBar from "./components/NavBar"
+import Feed from "./components/RandomFeed";
 
 function App() {
   return (
     <Box className="App">
       <NavBar></NavBar>
+        <Feed></Feed>
     </Box>
   );
 }
--- src/components/RandomFeed.jsx
+++ src/components/RandomFeed.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import {Grid} from "@mui/material";
 
 const Feed = () => {
+
+    const [images, setImages] = React.useState([]);
+
+    async function refreshImages() {
+        setImages([])
+
+        try {
+            const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=12', {headers: {'x-api-key': process.env.REACT_APP_CAT_API_KEY}});
+            const json = await response.json();
+            setImages(json);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
     return (
-        <div>
-            <h1></h1>
-        </div>
-    )
+            <Grid container columns={{xs:4, sm:8, md:12}}>
+                {
+                    images && images.map(image => (
+                            <Image data={image} key={image.id}/>
+                    ))
+                }
+            </Grid>
+    );
 }
+
+export default Feed;
